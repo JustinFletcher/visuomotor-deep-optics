39e2a907b4aaffbfa12beb2b6338a19194e09a79
Justin Fletcher <justinr.fletcher@gmail.com>
2025-07-05 12:38:14 -1000

    Fixed sav location issue.

--- optomech/merge_replay_buffers.py
+++ optomech/merge_replay_buffers.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
         args.parent_dir,
         filter_disadvantageous=args.filter_disadvantageous,
         explore_to_exploit_ratio=args.explore_to_exploit_ratio)
-    combined_path = os.path.join(args.parent_dir, "combined_replay_buffer.pt")
+    combined_path = os.path.join(args.parent_dir, args.output_dir)
     combined_rb.save(combined_path,
                      chunk_size=args.chunk_size,)
     print(f"Saved combined replay buffer to {combined_path}")
