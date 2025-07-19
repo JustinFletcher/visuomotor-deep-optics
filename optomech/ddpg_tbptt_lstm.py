7ba97700ee680825da2cb6c51d444c666a2e678c
Justin Fletcher <justinr.fletcher@gmail.com>
2025-07-19 10:40:44 -1000

    Bug fix.

--- optomech/ddpg_tbptt_lstm.py
+++ optomech/ddpg_tbptt_lstm.py
@@ -461,8 +461,12 @@ class ImpalaActor(nn.Module):
 
             r_prior = r_prior.unsqueeze(-1)
 
-            h0 = torch.tensor(hidden[0])  # → [1,4,128]
-            c0 = torch.tensor(hidden[1]) 
+            # h0 = torch.tensor(hidden[0])  # → [1,4,128]
+            # c0 = torch.tensor(hidden[1]) 
+
+            h0 = hidden[0]  # → [1,4,128]
+            c0 = hidden[1]
+
 
 
         x_o = self.visual_encoder(o)
