79a920a807baf16247da6aed1ac8d329c89199fa
Justin Fletcher <justinr.fletcher@gmail.com>
2025-10-04 20:15:17 +1000

    update.

--- optomech/supervised_ml/train_sml_model.py
+++ optomech/supervised_ml/train_sml_model.py
@@ -278,8 +278,8 @@ class SMLResNetGN(nn.Module):
         super().__init__()
         self.input_crop_size = input_crop_size
         # Input group default
-        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=3, stride=1, padding=3, bias=False)
-        self.gn1 = nn.GroupNorm(num_groups=16, num_channels=32)  # Use 16 groups for 32 channels
+        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=7, stride=1, padding=3, bias=False)
+        self.gn1 = nn.GroupNorm(num_groups=8, num_channels=32)  # Use 16 groups for 32 channels
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
         # Input group default
@@ -320,7 +320,7 @@ class SMLResNetGN(nn.Module):
         x = self.conv1(x)
         x = self.gn1(x)
         x = self.relu(x)
-        x = self.maxpool(x)
+        # x = self.maxpool(x)
         
         x = self.layer1(x)
         x = self.layer2(x)
