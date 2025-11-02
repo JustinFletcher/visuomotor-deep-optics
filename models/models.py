47a0bcc0c88109d9901325a2b4fd4712e07ffbfc
Justin Fletcher <justinr.fletcher@gmail.com>
2025-11-01 19:48:12 -1000

    Update.

--- models/models.py
+++ models/models.py
@@ -492,7 +492,7 @@ class ResNet18Actor(nn.Module):
         self.action_head = nn.Sequential(
             nn.Linear(self.feature_dim, 512),
             nn.ReLU(),
-            # nn.Dropout(0.1),
+            nn.Dropout(0.1),
             nn.Linear(512, action_dim),
             nn.Tanh()
         )
